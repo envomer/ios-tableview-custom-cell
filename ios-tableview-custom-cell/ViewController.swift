3489ca1458bd8b25ad6ce46131a563f9abebdf3a
Omer Mohamed Ali <om@anexia.at>
2020-09-21 11:24:09 +0200

    Initial Commit

---
+++ ios-tableview-custom-cell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-tableview-custom-cell
+//
+//  Created by Omer Mohamed Ali on 21.09.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
